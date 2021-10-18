358933380dfec1de422ce841d61f44c5ca826e43
Brian Colfer <brian.colfer@gmail.com>
2021-10-17 18:43:52 -0700

    Creating discussions and fixing trubo-rails install

---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+import "@hotwired/turbo-rails"
+import "./controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = true
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+//import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -13,3 +13,13 @@ Turbolinks.start()
 ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = true
+
+// From fomera course
+require("trix")
+require("@rails/actiontext")
